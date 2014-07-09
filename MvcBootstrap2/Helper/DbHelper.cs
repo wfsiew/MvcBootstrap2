c4ddb11493627cef8da5f09c1820466f5ecc2c38
wfsiew <siewwingfei@hotmail.com>
2014-07-09 17:31:45 +0800

    a

--- MvcBootstrap2/Helper/DbHelper.cs
+++ MvcBootstrap2/Helper/DbHelper.cs
@@ -16,7 +16,7 @@ namespace MvcBootstrap2.Helper
             {
                 if (db == null)
                 {
-                    var connectionString = "mongodb://localhost";
+                    var connectionString = "mongodb://appharbor_803b7e5d-de7d-474f-96a7-1f5d651c3ef0:o8o4usrnrb2d6rtihpd168oj54@ds061298.mongolab.com:61298/appharbor_803b7e5d-de7d-474f-96a7-1f5d651c3ef0";
                     var client = new MongoClient(connectionString);
                     var server = client.GetServer();
                     db = server.GetDatabase("appdb");
