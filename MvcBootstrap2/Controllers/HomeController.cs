b3bb7fdfce3de3bc1c5561346e930ee38acce8ed
wfsiew <siewwingfei@hotmail.com>
2014-07-09 23:39:02 +0800

    c

--- MvcBootstrap2/Controllers/HomeController.cs
+++ MvcBootstrap2/Controllers/HomeController.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MvcBootstrap2.Models;
+using MvcBootstrap2.ViewModels;
 using MvcBootstrap2.Helper;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using MongoDB.Driver.Linq;
 
 namespace MvcBootstrap2.Controllers
 {
@@ -31,8 +34,19 @@ namespace MvcBootstrap2.Controllers
         public ActionResult About()
         {
             ViewBag.menu = "About";
+            var students = Student.GetCollection();
+            List<EnrollmentDateGroup> v = new List<EnrollmentDateGroup>();
+            var l = students.Distinct("EnrollmentDate").OrderBy(x => x.ToUniversalTime());
+            foreach (var k in l)
+            {
+                EnrollmentDateGroup o = new EnrollmentDateGroup();
+                o.EnrollmentDate = k.ToUniversalTime();
+                var q = Query<Student>.Where(x => x.EnrollmentDate == o.EnrollmentDate);
+                o.StudentCount = students.Count(q);
+                v.Add(o);
+            }
 
-            return null;
+            return View(v);
         }
 	}
 }
---
+++ MvcBootstrap2/ViewModels/EnrollmentDateGroup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcBootstrap2.ViewModels
+{
+    public class EnrollmentDateGroup
+    {
+        [DataType(DataType.Date)]
+        public DateTime? EnrollmentDate { get; set; }
+
+        public long StudentCount { get; set; }
+    }
+}
